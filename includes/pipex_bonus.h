69da8945d105a337238af8983c849d77a262bb93
hebernar <hebernar@student.42.fr>
2023-05-25 16:08:13 +0200

    To be pushed

--- includes/pipex_bonus.h
+++ includes/pipex_bonus.h
@@ -6,7 +6,7 @@
 /*   By: hebernar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 00:52:48 by hebernar          #+#    #+#             */
-/*   Updated: 2023/05/24 00:53:17 by hebernar         ###   ########.fr       */
+/*   Updated: 2023/05/25 15:14:45 by hebernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,19 +28,19 @@
 # define BUFFER_SIZE 2048
 
 //pipex_bonus
-int main(int argc, char **argv, char **env);
-void ft_pipex(int argc, char **argv, char **env);
-void ft_here_doc(int argc, char **argv);
-void write_to_limiter(int *pipefd, char *limiter);
-int ft_fdopen(char *file, int flags);
+int		main(int argc, char **argv, char **env);
+void	ft_pipex(int argc, char **argv, char **env);
+void	ft_here_doc(int argc, char **argv);
+void	write_to_limiter(int *pipefd, char *limiter);
+int		ft_fdopen(char *file, int flags);
 
 //exec_bonus
 void	ft_execution(char *cmd, char **env);
 void	ft_exec_cmd(char *cmd, char **env);
 
 //validation_bonus
-int	is_cmd_in_path(char **command, char **path, char *cmd, int execute);
-int	is_cmd_valid(char *cmd, char **env);
+int		is_cmd_in_path(char **command, char **path, char *cmd, int execute);
+int		is_cmd_valid(char *cmd, char **env);
 char	*double_join(char **str, char **str2, int i);
 void	ft_bigfree(char **str, char **str2);
 char	**ft_path_split(char **env);
